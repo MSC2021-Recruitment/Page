c63bf281438590c2a6231103db38ae068ba219f0
cyk1464 <1464837318@qq.com>
2021-07-22 18:05:13 +0800

    commit on 'master'

--- 1.php
+++ 1.php
@@ -4,14 +4,14 @@ session_set_cookie_params($lifeTime);
 session_start();
 $stat = $_POST["stat"];
 
+$severname = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "msc";
+
 if ($stat == 1) {
     $q = $_POST["q"];
     $p = $_POST["p"];
-
-    $severname = "localhost";
-    $username = "root";
-    $password = "root";
-    $dbname = "msc";
     $conn = mysqli_connect($severname, $username, $password, $dbname);
     $result = mysqli_query($conn,"SELECT * FROM msc WHERE uname = '$q'");
     $row = mysqli_fetch_array($result);
@@ -24,33 +24,52 @@ if ($stat == 1) {
     
         if($p == $upassword) {
             $_SESSION['loged'] = 1;
+            $_SESSION['uid'] =  $row['id'];
             echo true;
         }
         else {
             echo false;
         }
     }
-/*
-    if ($q == 1 && $p == 1) {
-        $_SESSION['loged'] = 1;
-        echo 1;
-    }
-*/
 }
-if ($_SESSION['loged'] && $stat == 3) {
-    echo 1;
+if (!isset($_SESSION['loged']) && $stat == 3) {
+    echo true;
 }
 if ($stat == 2) {
-    $_SESSION['loged'] = 0;
+    session_destroy();
 }
-if($stat==4)
-{
-    $_SESSION['ans']=$_POST['text'];
-    $_SESSION['mode']=$_POST['mode'];
-    echo 0;
+if($stat==4) {
+    if(!isset($_SESSION['loged'])) {
+        echo false;
+    }
+    $conn = mysqli_connect($severname, $username, $password, $dbname);
+    
+    $uu = 'FL';
+    $unum = $uu . $_POST["num"];
+    $umodid = $unum . "mode";
+    $uans = $_POST["text"];
+    $umod = $_POST["mode"];
+    
+    $sql1 = "ALTER TABLE msc ADD $unum TEXT NOT NULL";
+    $sql2 = "ALTER TABLE msc ADD $umodid VARCHAR(30) NOT NULL";
+    
+    mysqli_query($conn,$sql1);
+    mysqli_query($conn,"UPDATE msc SET $unum = '$uans'
+    WHERE id='$uid' ");
+
+    mysqli_query($conn,$sql2);
+    mysqli_query($conn,"UPDATE msc SET $umodid = '$umod'
+    WHERE id='$uid' ");
+
+    
+    //$_SESSION['ans']=$_POST['text'];
+    //$_SESSION['mode']=$_POST['mode'];
+    echo true;
 }
-if($stat==5)
-{
+if($stat==5) {
+    if(!isset($_SESSION['loged'])) {
+        echo false;
+    }
     $ans= $_SESSION['ans'];
     $mode=$_SESSION['mode'];
     $a=array("ans"=>$ans,"mode"=>$mode);
