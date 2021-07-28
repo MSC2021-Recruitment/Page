7256bbc3e51659614a84945b7dc4cd6c8d38b9ac
GuChen <1023797278@qq.com>
2021-07-28 23:48:04 +0800

    update

--- details.php
+++ details.php
@@ -1,3 +1,14 @@
 <?php
-echo 1;
+echo '{
+    "name": "顾晨",
+    "number": "20009200044",
+    "email": "1023797278@qq.com",
+    "will": "Web组",
+    "major": "软件工程",
+    "intro": "大家好，我叫顾晨123kdsajjdasdjikjasddddddddddddddddddddddjsdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaakdsajjdasdjikjasddddddddddddddddddddddjsdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaakdsajjdasdjikjasddddddddddddddddddddddjsdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    "questions": {
+        "题1": "include<iostream>;\nint main(){\n  std::cout<<1;\n}    ",
+        "题2": "int main()"
+    }
+}';
 ?>
--- js/load.js
+++ js/load.js
@@ -55,11 +55,34 @@ function details(email) {
 
         },
         success: function(data) {
+            data = JSON.parse(data)
             $('#deten').empty();
-            $('#p1').fadeOut(300)
+            $('#deten').append('\
+                <div class="mdui-typo">\
+                    <div class="subheading mdui-col-md-4 mdui-col-xs-12 mdui-col-lg-3 mdui-col-sm-6">姓名:' + data.name + '</div>\
+                    <div class="subheading mdui-col-md-4 mdui-col-xs-12 mdui-col-lg-3 mdui-col-sm-6">邮箱:' + data.email + '</div>\
+                    <div class="subheading mdui-col-md-4 mdui-col-xs-12 mdui-col-lg-3 mdui-col-sm-6">学号:' + data.number + '</div>\
+                    <div class="subheading mdui-col-md-4 mdui-col-xs-12 mdui-col-lg-3 mdui-col-sm-6">意向:' + data.will + '</div>\
+                    <div class="subheading mdui-col-md-4 mdui-col-xs-12 mdui-col-lg-3 mdui-col-sm-6">专业:' + data.major + '</div>\
+                    <div class="subheading mdui-col-xs-12">个人介绍:<pre class="pre">' + data.intro + '</pre></div>\
+            ')
+            for (k in data.questions) {
+                a = data.questions[k]
+                a = a.replace("&", "&amp;")
+                a = a.replace("<", "&lt;")
+                a = a.replace(" ", "&nbsp;")
+                a = a.replace(">", "&gt;")
+                a = a.replace('"', '&quot;')
+                a = a.replace("'", "&apos;")
+                console.log(a)
+                $('#deten').append('<div class="subheading mdui-col-xs-12">' + k + ':<pre><code>' + a + '</code></pre></div>')
+            }
+            hljs.highlightAll();
+
+            $('#p1').fadeOut(250)
             setTimeout(function() {
-                $('#p3').fadeIn(300)
-            }, 500)
+                $('#p3').fadeIn(100)
+            }, 250)
 
 
         }
